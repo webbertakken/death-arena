7eee63197082ee9b79faaae23595eab8d9bfd274
Webber <webber@takken.io>
2022-11-06 15:32:42 +0100

    chore: allow numbers too

--- src/gameplay/arena/scene.rs
+++ src/gameplay/arena/scene.rs
@@ -22,9 +22,14 @@ pub struct Scale {
     pub z: f32,
 }
 
+#[allow(clippy::cast_possible_truncation)]
 fn string_to_float<'de, D: Deserializer<'de>>(deserializer: D) -> Result<f32, D::Error> {
     Ok(match Value::deserialize(deserializer)? {
         Value::String(s) => s.parse().map_err(de::Error::custom)?,
+        Value::Number(num) => {
+            num.as_f64()
+                .ok_or_else(|| de::Error::custom("Invalid number"))? as f32
+        }
         _ => return Err(de::Error::custom("wrong type")),
     })
 }
