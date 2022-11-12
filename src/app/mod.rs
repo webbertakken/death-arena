dd7c99cd930a6d6c90a41ada5b00509aeb1db5c2
Webber Takken <webber@takken.io>
2022-11-12 23:37:44 +0100

    feat: add physics (#17)
    * feat: add physics
    
    * chore: cleanup
    
    * chore: add colliders to car parts
    
    * chore: correct syntax for rigidbody with children

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,12 +1,14 @@
+use crate::app::configuration::ConfigurationPlugin;
 use crate::app::generic::GenericSystemsPlugin;
 use crate::app::lights::LightingPlugin;
 use crate::ui::UserInterfacePlugin;
 use bevy::app::PluginGroupBuilder;
 use bevy::prelude::*;
-use bevy::prelude::*;
 use bevy_kira_audio::AudioPlugin;
+use bevy_rapier2d::prelude::*;
 use init::InitPlugin;
 
+pub mod configuration;
 pub mod generic;
 pub mod init;
 pub mod lights;
@@ -19,5 +21,8 @@ impl PluginGroup for AppPlugins {
         group.add(LightingPlugin::default());
         group.add(GenericSystemsPlugin::default());
         group.add(UserInterfacePlugin::default());
+        group.add(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0));
+        group.add(RapierDebugRenderPlugin::default());
+        group.add(ConfigurationPlugin::default());
     }
 }
--- src/gameplay/player/car.rs
+++ src/gameplay/player/car.rs
@@ -1,6 +1,7 @@
 use crate::gameplay::player::Player;
 use bevy::prelude::*;
 use bevy_inspector_egui::Inspectable;
+use bevy_rapier2d::prelude::*;
 
 #[derive(Component)]
 pub struct FrontLeftWheel;
@@ -21,89 +22,100 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Wheel2 needs approx position of -132, 135 (front) -188 (rear), and scale 0.7
 
     commands
-        .spawn_bundle(SpriteBundle {
-            texture: player_handle,
-            transform: Transform {
-                translation: Vec3::new(-430.0, 0.0, 5.0),
-                rotation: Quat::from_rotation_z(f32::to_radians(8.0)),
-                scale: Vec3::new(0.2, 0.2, 0.0),
-            },
-            ..default()
-        })
+        .spawn()
         .insert(Name::new("Player"))
         .insert(Player {
             movement_speed: 500.0,                  // metres per second
             rotation_speed: f32::to_radians(360.0), // degrees per second
         })
+        .insert_bundle(SpriteBundle {
+            texture: player_handle,
+            transform: Transform {
+                translation: Vec3::new(-430.0, 0.0, 3.0),
+                rotation: Quat::from_rotation_z(f32::to_radians(8.0)),
+                scale: Vec3::new(0.2, 0.2, 0.2),
+            },
+            ..default()
+        })
+        .insert(RigidBody::Dynamic)
+        .insert(Velocity::zero())
+        .insert(Collider::ball(350.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(Ccd::enabled())
         .with_children(|parent| {
             // Front left wheel
             parent
-                .spawn_bundle(SpriteBundle {
+                .spawn()
+                .insert(Name::new("Wheel (FL)"))
+                .insert(FrontLeftWheel)
+                .insert_bundle(SpriteBundle {
                     texture: wheel_handle.clone(),
                     transform: Transform {
-                        translation: Vec3::new(-117.0, 128.0, 4.9),
+                        translation: Vec3::new(-117.0, 128.0, 0.0),
                         rotation: Quat::from_rotation_z(f32::to_radians(0.0)),
-                        scale: Vec3::new(1.1, 1.1, 0.0),
+                        scale: Vec3::new(1.1, 1.1, 1.1),
                     },
                     ..default()
-                })
-                .insert(Name::new("Wheel (FL)"))
-                .insert(FrontLeftWheel);
+                });
 
             // Front right wheel
             parent
-                .spawn_bundle(SpriteBundle {
+                .spawn()
+                .insert(Name::new("Wheel (FR)"))
+                .insert(FrontRightWheel)
+                .insert_bundle(SpriteBundle {
                     texture: wheel_handle.clone(),
                     transform: Transform {
-                        translation: Vec3::new(114.0, 128.0, 4.9),
+                        translation: Vec3::new(114.0, 128.0, 0.0),
                         rotation: Quat::from_rotation_z(f32::to_radians(0.0)),
-                        scale: Vec3::new(1.1, 1.1, 0.0),
+                        scale: Vec3::new(1.1, 1.1, 1.01),
                     },
                     ..default()
-                })
-                .insert(Name::new("Wheel (FR)"))
-                .insert(FrontRightWheel);
+                });
 
             // Rear left wheel
             parent
-                .spawn_bundle(SpriteBundle {
+                .spawn()
+                .insert(Name::new("Wheel (RL)"))
+                .insert(RearLeftWheel)
+                .insert_bundle(SpriteBundle {
                     texture: wheel_handle.clone(),
                     transform: Transform {
-                        translation: Vec3::new(-115.0, -167.5, 4.9),
+                        translation: Vec3::new(-115.0, -167.5, 0.0),
                         rotation: Quat::from_rotation_z(f32::to_radians(0.0)),
-                        scale: Vec3::new(1.2, 1.2, 0.0),
+                        scale: Vec3::new(1.2, 1.2, 1.2),
                     },
                     ..default()
-                })
-                .insert(Name::new("Wheel (RL)"))
-                .insert(RearLeftWheel);
+                });
 
             // Rear right wheel
             parent
-                .spawn_bundle(SpriteBundle {
+                .spawn()
+                .insert(Name::new("Wheel (RR)"))
+                .insert(RearRightWheel)
+                .insert_bundle(SpriteBundle {
                     texture: wheel_handle.clone(),
                     transform: Transform {
-                        translation: Vec3::new(115.0, -167.5, 4.9),
+                        translation: Vec3::new(115.0, -167.5, 0.0),
                         rotation: Quat::from_rotation_z(f32::to_radians(0.0)),
-                        scale: Vec3::new(1.2, 1.2, 0.0),
+                        scale: Vec3::new(1.2, 1.2, 1.2),
                     },
                     ..default()
-                })
-                .insert(Name::new("Wheel (RR)"))
-                .insert(RearRightWheel);
+                });
 
             // Spikes
             parent
-                .spawn_bundle(SpriteBundle {
+                .spawn()
+                .insert(Name::new("Spikes"))
+                .insert(SpikesUpgrade)
+                .insert_bundle(SpriteBundle {
                     texture: spikes_handle,
                     transform: Transform {
-                        translation: Vec3::new(0.0, 225.0, 5.2),
+                        translation: Vec3::new(0.0, 225.0, 0.0),
                         rotation: Quat::from_rotation_z(f32::to_radians(0.0)),
-                        scale: Vec3::new(1.0, 1.0, 0.0),
+                        scale: Vec3::new(1.0, 1.0, 1.0),
                     },
                     ..default()
-                })
-                .insert(Name::new("Spikes"))
-                .insert(SpikesUpgrade);
+                });
         });
 }
